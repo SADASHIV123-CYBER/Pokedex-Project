a23c7133f3e2c7fb4b04c1545de1993e8bc882ec
sadashiv <sadashivkale604@gmail.com>
2025-01-16 23:24:11 +0530

    added routing

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,22 +1,19 @@
-// import React from 'react';
-// import PropTypes from 'prop-types'; // Import PropTypes for prop validation
+import { Link } from 'react-router-dom';
 import './Pokemon.css';
 
-function Pokemon({ name, image }) {
+function Pokemon({ name, image, id }) {
     return (
+        <Link to={`/pokemon/${id}`} >
         <div className='pokemon'>
             <div>{name}</div>
             <div>
                 <img src={image} className='pokemon-image' alt={name} />
             </div>
         </div>
+        </Link>
     );
 }
 
-// Prop validation for the 'name' and 'image' props
-// Pokemon.propTypes = {
-//     name: PropTypes.string.isRequired,  // name must be a string and is required
-//     image: PropTypes.string.isRequired, // image must be a string and is required
-// };
+
 
 export default Pokemon;
